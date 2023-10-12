72695eabff0cd84dc7c66cba350e6759ee69a349
Herasymova <m.v.herasymova@student.khai.edu>
2023-10-12 20:33:36 +0300

    ChatBot 1st stage
    ChatBot 1st stage
    
    (cherry picked from commit a31ebd59128d98a1bdc189e921a0c10f174ccd5b)

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,13 @@
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello! My name is SomeBot");
+        System.out.println("I was created in 2023");
+
+
+    }
+}
