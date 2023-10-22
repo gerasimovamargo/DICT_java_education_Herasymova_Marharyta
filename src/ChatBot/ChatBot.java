12a7161ded0535b5a308a92bdd3d4422320a00c3
Herasymova <m.v.herasymova@student.khai.edu>
2023-10-22 23:11:00 +0300

    ChatBot 1st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,13 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello! My name is SomeBot");
+        System.out.println("I was created in 2023");
+
+
+    }
 }
