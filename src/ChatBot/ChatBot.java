922faab95dbc674a2d4d98eba2618f67e38fe7d4
Herasymova <m.v.herasymova@student.khai.edu>
2023-10-22 23:12:25 +0300

    ChatBot 2nd stage
    (cherry picked from commit edc252e757ae160f9c60d7998f6002344c34059a)

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,6 +8,9 @@ public class ChatBot {
         System.out.println("Hello! My name is SomeBot");
         System.out.println("I was created in 2023");
 
-
+        System.out.println("Please remind me your name.");
+        Scanner in = new Scanner(System.in);
+        String answer = in.nextLine();
+        System.out.printf("What a great name you have, %s!%n", answer);
     }
 }
