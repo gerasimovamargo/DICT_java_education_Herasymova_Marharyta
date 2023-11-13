4924f6a1b2d79e5700ca77ec0e7c40bc330ba51e
Herasymova <m.v.herasymova@student.khai.edu>
2023-11-13 23:43:00 +0200

    ChatBot 6th stage
    Resolved merge conflict by incorporating both suggestions
    
    ChatBot 6th stage
    
    Resolved merge conflict by incorporating both suggestions
    
    ChatBot 6th stage
    
    Resolved merge conflict by incorporating both suggestions
    
    ChatBot 6th stage
    
    Resolved merge conflict by incorporating both suggestions

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -7,56 +7,67 @@ public class Hangman {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
 
-
-        String [] answers = {"python", "java", "javascript", "kotlin"};
+        String[] answers = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
-        int select = random.nextInt(answers.length);
-        String correctAnswer = answers[select];
 
-        char[] guessedLetters = new char[correctAnswer.length()];
-        for (int i = 0; i < guessedLetters.length; i++) {
-            guessedLetters[i] = '-';
-        }
-
-        int remainingAttempts = 8;
-
-        while (remainingAttempts > 0) {
-            System.out.println("Guessed word: " + new String(guessedLetters));
-            System.out.println("Attempts left: " + remainingAttempts);
-
-            System.out.print("Input a letter: ");
+        String state;
+        while (true) {
+            int select = random.nextInt(answers.length);
+            String correctAnswer = answers[select];
             Scanner scanner = new Scanner(System.in);
-            char inputLetter = scanner.nextLine().charAt(0);
-
-            boolean letterGuessed = false;
-            for (int i = 0; i < correctAnswer.length(); i++) {
-                if (correctAnswer.charAt(i) == inputLetter) {
-                    guessedLetters[i] = inputLetter;
-                    letterGuessed = true;
-                }
+            int lives = 8;
+            StringBuilder line = new StringBuilder();
+            line.append("-".repeat(correctAnswer.length()));
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: ");
+            state = scanner.nextLine();
+            if (state.equals("exit")) {
+                break;
+            } else if (!state.equals("play")) {
+                System.out.println("Unrecognized command, repeat please");
+                continue;
             }
-
-            if (!letterGuessed) {
-                System.out.println("That letter doesn't appear in the word");
-                remainingAttempts--;
-            }
-
-            boolean allLettersGuessed = true;
-            for (char letter : guessedLetters) {
-                if (letter == '-') {
-                    allLettersGuessed = false;
+            while (lives > 0) {
+                System.out.printf("The word is %s!%n", line);
+                if (!line.toString().contains("-")) {
+                    System.out.println("You guessed the word!\n" +
+                     "You survived!");
                     break;
                 }
+                System.out.printf("You currently have %s lives!%n", lives);
+                System.out.print("Enter the letter: ");
+                String userAnswer = scanner.nextLine();
+                if (userAnswer.length() != 1) {
+                    System.out.println("You should input a single letter");
+                    continue;
+                }
+                char userChar = userAnswer.charAt(0);
+                if (Character.isUpperCase(userChar)) {
+                    System.out.println("Please enter a lowercase English letter");
+                    continue;
+                }
+                if (line.toString().contains(String.valueOf(userChar))) {
+                    System.out.println("You've already guessed this letter.");
+                } else if (!correctAnswer.contains(String.valueOf(userChar))) {
+                    System.out.println("You lost!");
+                    lives--;
+                } else {
+                    StringBuilder subLine = new StringBuilder();
+                    for (int i = 0; i < correctAnswer.length(); i++) {
+                        if (correctAnswer.charAt(i) == userChar) {
+                            subLine.append(userChar);
+                        } else if (line.charAt(i) != '-') {
+                            subLine.append(line.charAt(i));
+                        } else {
+                            subLine.append("-");
+                        }
+                    }
+                    line = new StringBuilder(subLine.toString());
+                }
+            }
+            if (lives == 0) {
+                System.out.println("Thanks for playing!");
             }
 
-            if (allLettersGuessed) {
-                System.out.println("Congratulations! You guessed the word: " + correctAnswer);
-                break;
-            }
-        }
-
-        if (remainingAttempts == 0) {
-            System.out.println("Sorry, you ran out of attempts. The correct word was: " + correctAnswer);
         }
     }
 }
