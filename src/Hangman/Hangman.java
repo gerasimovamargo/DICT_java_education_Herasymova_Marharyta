1094cfd1554e0fbaccf50be26ee550bbf3ad0720
Herasymova <m.v.herasymova@student.khai.edu>
2023-11-13 20:23:29 +0200

    ChatBot 2nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,8 +1,26 @@
 package Hangman;
+import java.util.Scanner;
+
 
 public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
+
+        String correctAnswer = "java";
+          System.out.print("Guess the word: ");
+                Scanner scanner = new Scanner(System.in);
+                String userAnswer = scanner.nextLine();
+
+        if (userAnswer.equals(correctAnswer)) {
+            System.out.println("You survived!");
+        } else {
+            while (!userAnswer.equals(correctAnswer)) {
+                System.out.println("You lost!");
+                System.out.print("Guess the word: ");
+                userAnswer = scanner.nextLine();
+            }
+            System.out.println("You survived!");
+        }
     }
 }
