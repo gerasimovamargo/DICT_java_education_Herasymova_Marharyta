804b538390741e31411acc9b3488e3f940e1b71d
Herasymova <m.v.herasymova@student.khai.edu>
2023-11-13 20:42:55 +0200

    ChatBot 3rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,13 +1,17 @@
 package Hangman;
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
 
-        String correctAnswer = "java";
+
+        String [] answers = {"python", "java", "javascript", "kotlin"};
+        Random random = new Random();
+        int select = random.nextInt(answers.length);
+        String correctAnswer = answers[select];
           System.out.print("Guess the word: ");
                 Scanner scanner = new Scanner(System.in);
                 String userAnswer = scanner.nextLine();
